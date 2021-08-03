c91b55ff6ded2330e9ea0ed0661de908cf360379
AmanPant777 <footballeramanpt65@gmail.com>
2021-08-03 18:29:47 +0530

    Add sidebar chats through icon in firebase

--- src/comp/SideBar.jsx
+++ src/comp/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './SideBar.css'
 import Avatar from '@material-ui/core/Avatar';
 import SearchIcon from '@material-ui/icons/Search';
@@ -7,24 +7,43 @@ import RateReviewIcon from '@material-ui/icons/RateReview';
 import SideBarChat from './SideBarChat'
 import { useSelector } from 'react-redux';
 import { selectUser } from '../redux/Slice/userSlice'
+import db, { auth } from '../firebase'
 const SideBar = () => {
     const user = useSelector(selectUser)
+    const [chats, setChats] = useState([])
+    const addChat = () => {
+        const ChatName = prompt('Please enter a chat Name')
+        if (ChatName) {
+            db.collection('chats').add({
+                ChatName: ChatName
+            })
+        }
+    }
+    useEffect(() => {
+        db.collection('chats').onSnapshot(snapshot => {
+            return setChats(snapshot.docs.map(doc => ({
+                id: doc.id,
+                data: doc.data()
+            })))
+        })
+    }, [])
+
     return (
         <div className="sidebar">
             <div className="sidebar_header">
-                <Avatar className='sidebar_avatar' src={user.photo} />
+                <Avatar className='sidebar_avatar' src={user.photo} onClick={() => auth.signOut()} />
                 <div className="sidebar_header_input">
                     <SearchIcon />
                     <input type="text" placeholder='Search' />
                 </div>
                 <IconButton className='sidebar_chats' variant='outlined'>
-                    <RateReviewIcon />
+                    <RateReviewIcon onClick={addChat} />
                 </IconButton>
             </div>
             <div className="sidebar_chats">
-                <SideBarChat />
-                <SideBarChat />
-
+                {chats.map(({ id, data }) =>
+                    <SideBarChat Name={data.ChatName} key={id} id={id} />
+                )}
             </div>
         </div>
     )
--- src/comp/SideBarChat.jsx
+++ src/comp/SideBarChat.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './SideBarChat.css'
 import Avatar from '@material-ui/core/Avatar';
-const SideBarChat = () => {
+const SideBarChat = ({ Name, id }) => {
     return (
         <div className="sidebarchat">
             <Avatar />
             <div className="sidebarchat_info">
-                <h3>Channel name</h3>
+                <h3>{Name}</h3>
                 <p>Last Seen Message</p>
                 <small>Timestamp</small>
             </div>
