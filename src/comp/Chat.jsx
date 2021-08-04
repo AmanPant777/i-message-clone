76c64a7036c2094735d6fd8e80cafdb8302c9ed7
AmanPant777 <footballeramanpt65@gmail.com>
2021-08-04 16:33:56 +0530

    add message to firebase and recieve it in Mesage

--- src/comp/Chat.jsx
+++ src/comp/Chat.jsx
@@ -1,21 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Chat.css'
 import MicNoneIcon from '@material-ui/icons/MicNone';
 import IconButton from '@material-ui/core/IconButton'
 import Message from './Message'
-import { selectChat } from '../redux/Slice/chatSlice'
+import { selectChat, selectId } from '../redux/Slice/chatSlice'
 import { useSelector } from 'react-redux';
+import { selectUser } from '../redux/Slice/userSlice';
+import db from '../firebase';
+import firebase from 'firebase';
 const Chat = () => {
     const [input, setInput] = useState('')
+    const [messages, setMessages] = useState([])
+    const ChatId = useSelector(selectId)
+    const user = useSelector(selectUser)
+    const sendMessage = (e) => {
+        e.preventDefault()
+        db.collection('chats').doc(ChatId).collection('messages').add({
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            message: input,
+            uid: user.uid,
+            photo: user.photo,
+            email: user.email,
+            displayName: user.displayName
+        })
+        setInput('')
+    }
+    useEffect(() => {
+        if (ChatId) {
+            db.collection('chats')
+                .doc(ChatId)
+                .collection('messages')
+                .orderBy('timestamp', 'desc')
+                .onSnapshot(snapshot => {
+                    setMessages(snapshot.docs.map(doc => ({
+                        id: doc.id,
+                        data: doc.data()
+                    })))
+                }
+
+                )
+        }
+    }, [ChatId])
     const handleChange = (e) => {
         e.preventDefault()
         setInput(e.target.value)
-        console.log(input)
+
     }
-    const sendMessage = (e) => {
-        e.preventDefault()
-        setInput('')
-    }
+
     const ChatName = useSelector(selectChat)
     return (
         <div className="chat">
@@ -24,7 +55,9 @@ const Chat = () => {
                 <strong>Details</strong>
             </div>
             <div className="chat_message">
-                <Message />
+                {messages.map((data, key) => <Message key={key} content={data} />
+
+                )}
             </div>
             <div className="chat_input">
                 <form >
--- src/comp/Message.jsx
+++ src/comp/Message.jsx
@@ -1,12 +1,13 @@
 import { Avatar } from '@material-ui/core'
 import React from 'react'
 import './Message.css'
-const Message = () => {
+const Message = ({ content: { id, data } }) => {
+
     return (
         <div className="message">
-            <Avatar />
-            <p>This is a message</p>
-            <small>times</small>
+            <Avatar src={data.photo} />
+            <p>{data.message}</p>
+            <small>{new Date(data.timestamp?.toDate()).toLocaleString()}</small>
         </div>
     )
 }
